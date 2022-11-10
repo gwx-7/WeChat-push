aba6bb5f2747e09066fd724a81b24fdbad50f88c
gwx-7 <84131920+gwx-7@users.noreply.github.com>
2022-11-10 20:47:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def get_weather(region):
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     key = config["weather_key"]
-    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
+    region_url = "https://free-api.heweather.com/s6/weather/forecast?location={}&key={}".format(region, key)
     response = get(region_url, headers=headers).json()
     if response["code"] == "404":
         print("推送消息失败，请检查地区名是否有误！")
