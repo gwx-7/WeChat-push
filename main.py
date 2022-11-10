1135deab35394402fd83e9794bea036c2ac481b7
gwx-7 <84131920+gwx-7@users.noreply.github.com>
2022-11-10 21:08:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def get_weather(region):
     print(response)
    
     # 获取地区的location--id
-    location_id = response['HeWeather6'][0]["id"]
+    location_id = response['HeWeather6'][0]["cid"]
     weather_url = "https://free-api.heweather.com/s6/weather/forecast?location={}&key={}".format(location_id, key)
     response = get(weather_url, headers=headers).json()
     # 天气
